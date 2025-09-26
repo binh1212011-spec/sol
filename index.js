5345fdff1699008c01feda08e68531d33fc73d0a
binh1212011-spec <binh1212011@gmail.com>
2025-09-26 13:26:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,26 +2,29 @@
 // Created by @mongoo.se
 // Last edited 06/05/2025
 
+require('dotenv').config();
 const WebSocket = require('ws');
 const { WebhookClient, Events, EmbedBuilder } = require('discord.js');
 
-const { token, webhookURL, gatewayURL, maxReconnectInterval, overrideUsername, overrideAvatarURL, colors, emojis } = require('./config.json');
+// lấy config từ file config.json
+const { webhookURL, gatewayURL, maxReconnectInterval, overrideUsername, overrideAvatarURL, colors, emojis } = require('./config.json');
+
+// token lấy từ .env
+const token = process.env.TOKEN;
 
 let reconnectInterval = 5_000;
 
-const webhookClient = new WebhookClient({ // Use the webhook URL to login to the webhook client
+const webhookClient = new WebhookClient({
     url: webhookURL
 });
 
 webhookClient.on(Events.Error, (error) => {
-    console.error(`Webhook client error: ${error.message}`)
+    console.error(`Webhook client error: ${error.message}`);
 });
 
 const connect = () => {
-    const ws = new WebSocket(gatewayURL, { // Create a WS connetion
-        headers: {
-            token: token
-        }
+    const ws = new WebSocket(gatewayURL, {
+        headers: { token }
     });
 
     ws.on('open', () => {
@@ -33,45 +36,52 @@ const connect = () => {
                 const connectedEmbed = new EmbedBuilder()
                     .setDescription(`${emojis.success} **Sol's Stat Tracker** - Connected`)
                     .setColor(colors.success);
-    
-                webhookClient.send({ embeds: [ connectedEmbed ] });
+
+                webhookClient.send({ embeds: [connectedEmbed] });
             }
         }, 1_000);
     });
 
     ws.on('message', (rawData) => {
         try {
-            rawData = JSON.parse(rawData.toString('utf8')); // Read the JSON data from the buffer
+            rawData = JSON.parse(rawData.toString('utf8'));
 
             switch (rawData.action) {
                 case 'enabled':
-                    const enabledEmbed = new EmbedBuilder()
-                        .setDescription(`${emojis.success} **Sol's Stat Tracker** - Enabled`)
-                        .setColor(colors.success);
-
-                    webhookClient.send({ embeds: [ enabledEmbed ] });
+                    webhookClient.send({
+                        embeds: [
+                            new EmbedBuilder()
+                                .setDescription(`${emojis.success} **Sol's Stat Tracker** - Enabled`)
+                                .setColor(colors.success)
+                        ]
+                    });
                     break;
+
                 case 'disabled':
-                    const disabledEmbed = new EmbedBuilder()
-                        .setDescription(`${emojis.error} **Sol's Stat Tracker** - Disabled`)
-                        .setColor(colors.error);
-
-                    webhookClient.send({ embeds: [ disabledEmbed ] });
+                    webhookClient.send({
+                        embeds: [
+                            new EmbedBuilder()
+                                .setDescription(`${emojis.error} **Sol's Stat Tracker** - Disabled`)
+                                .setColor(colors.error)
+                        ]
+                    });
                     break;
+
                 case 'executeWebhook':
-                    rawData.data.username = overrideUsername ?? rawData.data.username; // Set overrides
+                    rawData.data.username = overrideUsername ?? rawData.data.username;
                     rawData.data.avatarURL = overrideAvatarURL ?? rawData.data.avatarURL;
-                    rawData.allowedMentions = { parse: [] }; // Under no circumstances should the webhook be able to send mentions clea
+                    rawData.allowedMentions = { parse: [] };
 
-                    webhookClient.send(rawData.data); // Send the message payload
+                    webhookClient.send(rawData.data);
                     break;
+
                 default:
                     console.error(`WS client invalid action: ${rawData.action}`);
                     break;
             }
         } catch (error) {
             console.error(`WS client message error: ${error.message}`);
-        };
+        }
     });
 
     ws.on('close', async (code, reason) => {
@@ -81,41 +91,48 @@ const connect = () => {
         switch (code) {
             case 1008:
                 if (reason === 'Unauthorized') {
-                    const unauthorizedEmbed = new EmbedBuilder()
-                    .setTitle(`${emojis.error} Unauthorized`)
-                        .setDescription(`The API token is invalid.`)
-                        .setColor(colors.error);
-
-                    await webhookClient.send({ embeds: [ unauthorizedEmbed ] });
+                    await webhookClient.send({
+                        embeds: [
+                            new EmbedBuilder()
+                                .setTitle(`${emojis.error} Unauthorized`)
+                                .setDescription(`The API token is invalid.`)
+                                .setColor(colors.error)
+                        ]
+                    });
                 } else if (reason === 'Duplicate connection') {
-                    const unauthorizedEmbed = new EmbedBuilder()
-                        .setTitle(`${emojis.error} Duplicate Connection`)
-                            .setDescription(`The API token is already in-use.`)
-                            .setColor(colors.error);
-
-                    await webhookClient.send({ embeds: [ unauthorizedEmbed ] });
+                    await webhookClient.send({
+                        embeds: [
+                            new EmbedBuilder()
+                                .setTitle(`${emojis.error} Duplicate Connection`)
+                                .setDescription(`The API token is already in-use.`)
+                                .setColor(colors.error)
+                        ]
+                    });
                 }
-
                 break;
-                
-            case 1000:
-                const deletedEmbed = new EmbedBuilder()
-                .setTitle(`${emojis.error} Deleted`)
-                    .setDescription(`The API token has been deleted.`)
-                    .setColor(colors.error);
 
-            await webhookClient.send({ embeds: [ deletedEmbed ] });
-            break;
+            case 1000:
+                await webhookClient.send({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setTitle(`${emojis.error} Deleted`)
+                            .setDescription(`The API token has been deleted.`)
+                            .setColor(colors.error)
+                    ]
+                });
+                break;
 
             default:
                 console.log(`Reconnecting WS client in ${reconnectInterval}ms...`);
                 setTimeout(connect, reconnectInterval);
 
-                const reconnectingEmbed = new EmbedBuilder()
-                    .setDescription(`${emojis.none} **Sol's Stat Tracker** - Reconnecting`)
-                    .setColor(colors.none);
-        
-                await webhookClient.send({ embeds: [ reconnectingEmbed ] });
+                await webhookClient.send({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setDescription(`${emojis.none} **Sol's Stat Tracker** - Reconnecting`)
+                            .setColor(colors.none)
+                    ]
+                });
 
                 reconnectInterval = Math.min(maxReconnectInterval, reconnectInterval * 2);
                 break;
